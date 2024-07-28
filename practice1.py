1b4f970a350b1832c3127e0567112f4af594e566
SAN <sardarahmednazar@gmail.com>
2024-07-28 20:07:02 +0500

    done

---
+++ practice1.py
@@ -0,0 +1,14 @@
+print(-7//3)
+
+print(" my name is Ahmed \n my father name is Nazar")
+print("""
+    My name is Ahmed
+    My father name is Nazar
+      """)
+
+print(' how\'s it going ')
+
+print('1', end=' ')
+print('2', end=' ')
+print('3', end=' ')
+print('4', end=' ')
