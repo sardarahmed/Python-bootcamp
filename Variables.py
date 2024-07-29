ef01143f134475d610731333902e255f89bbbe55
SAN <sardarahmednazar@gmail.com>
2024-07-29 19:31:38 +0500

    done

---
+++ Variables.py
@@ -0,0 +1,59 @@
+print(5)
+
+radius = 2.5
+pi = 3.14
+
+area_of_circle = pi * radius**2
+
+print(area_of_circle)
+
+# Numeric Types
+int_value = 5
+float_value = 3.14
+complex_value = 2 + 3j
+
+# Sequence Types
+string_value = "Hello, world!"
+list_value = [1, 2, 3, 4, 5]
+tuple_value = (1, 2, 3, 4, 5)
+range_value = range(10)
+
+# Set Types
+set_value = {1, 2, 3, 4, 5}
+frozenset_value = frozenset([1, 2, 3, 4, 5])
+
+# Mapping Type
+dict_value = {'name': 'Ahmed', 'age': 23}
+
+# Boolean Type
+bool_value = True
+
+# Binary Types
+bytes_value = b'hello'
+bytearray_value = bytearray(b'hello')
+memoryview_value = memoryview(b'hello')
+
+# None Type
+none_value = None
+
+
+str1 = 'Ahmed'
+str2 = 'Nazar'
+
+concat = str1 + ' ' + str2
+print(concat)
+
+my_age = int(input('what is your age ? \n'))
+
+print('my age is ', my_age)
+
+
+circle_radius = float(input('Enter circle radius \n'))
+
+print('Area of circle is ', 3.14*circle_radius**2)
+
+# convert fahrenheit to celcius
+
+fahrenheit = float(input('Enter temperature in fahrenheit \n'))
+
+print ('Temp in fahrenheit: ', fahrenheit, ' in celcius = ', (fahrenheit-32)*5/9)
--- practice1.py
+++ practice1.py
@@ -1,14 +1,25 @@
+#Floor division
 print(-7//3)
+#-3
+
+
+print(8//4)
+# 2
+
 
 print(" my name is Ahmed \n my father \a name is Nazar")
 print("""
     My name is Ahmed
     My father name is Nazar
       """)
+#     My name is Ahmed
+#     My father name is Nazar
 
 print(' how\'s it going ')
+# how's it going
 
 print('1', end=' ')
 print('2', end=' ')
 print('3', end=' ')
 print('4', end=' ')
+## 1 2 3 4 
